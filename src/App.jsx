bfc1cb623da49fb40e0cb891ea4941d54bf12000
Kavish-12345 <kavishsrivastava37@gmail.com>
2025-06-11 00:28:07 +0530

    minor issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Home from './Home'
 import Buyer from './Buyer' // Make sure this component exists
 import './App.css'
 import Supplier from './Supplier'
 import Investor from './Investor'
 
+// ScrollToTop component
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
   return (
     <Router>
       <div className="App">
+        <ScrollToTop /> {/* Add this component */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/buyer" element={<Buyer />} />
